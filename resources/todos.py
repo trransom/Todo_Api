36d90f1262bdbef780106cfeb3e27098b0ac2f6a
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-09-12 18:21:17 -0400

    Added resources to todos.py

--- resources/todos.py
+++ resources/todos.py
@@ -1,9 +1,32 @@
-from flask import jsonify
+from flask import jsonify, Blueprint
 
-from flask.ext.restful import Resource
+from flask.ext.restful import Resource, Api
 
 import models
 
 class TodoList(Resource):
 	def get(self);
 		return jsonify({'todos': [{'name': 'Get Milk'}]})
+		
+class Todo(Resource):
+	def get(self, id):
+		return jsonify({'name': 'Get Milk'}]})
+		
+	def put(self, id):
+		return jsonify({'name': 'Get Milk'}]})
+		
+	def delete(self, id):
+		return jsonify({'name': 'Get Milk'}]})
+		
+todos_api = Blueprint('resources.courses', __name__)
+api = Api(todos_api)
+api.add_resource(
+	TodoList,
+	'/api/v1/todos',
+	endpoint='todos'
+)
+api.add_resource(
+	Todo,
+	'/api/v1/todos/<int:id>',
+	endpoint='todo'
+)
