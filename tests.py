9fcf9602e46f6f33739b7a042e2f548b69c729f2
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-09-13 18:06:56 -0400

    Added test for homepage view

--- tests.py
+++ tests.py
@@ -1,1 +1,11 @@
 import unittest
+
+from app import app
+from models import Todo
+
+
+class ViewTests(unittest.TestCase):
+	def test_homepage(self):
+		view = self.app.get('/')
+		self.assertEqual(view.status_code, 200)
+		
