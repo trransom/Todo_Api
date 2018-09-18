b1e50df41c4e8afa0285d696a8793d352abce407
Thomas Ransom <32624861+trransom@users.noreply.github.com>
2018-09-17 20:14:17 -0400

    Fixed get route in TodoList resource to return todos

--- resources/todos.py
+++ resources/todos.py
@@ -7,12 +7,14 @@ import models
 
 todo_fields = {
 	'id': fields.Integer,
-	'name': fields.String
+	'name': fields.String,
+	'url': fields.String,
+	'todos': fields.List(fields.String)
 }
 
-def todo_or_404(course_id):
+def todo_or_404(todo_id):
 	try:
-		course = models.Todo.get(models.Todo.id==todo_id)
+		todo = models.Todo.get(models.Todo.id==todo_id)
 	except models.Todo.DoesNotExist:
 		abort(404)
 	else:
@@ -24,7 +26,7 @@ class TodoList(Resource):
 		self.reqparse.add_argument(
 			'name',
 			required=True,
-			help='No task title provided',
+			help='No task name provided',
 			location=['form', 'json']
 		)
 		super().__init__()
@@ -32,7 +34,7 @@ class TodoList(Resource):
 	def get(self):
 		todos = [marshal(todo, todo_fields) 
 				for todo in models.Todo.select()]
-		return {'todos': todos}
+		return todos
 		
 	@marshal_with(todo_fields)
 	def post(self):
@@ -54,30 +56,30 @@ class Todo(Resource):
 		
 	@marshal_with(todo_fields)
 	def get(self, id):
-		return add_reviews(todo_or_404(id))
+		return add_todos(todo_or_404(id))
 		
 	@marshal_with(todo_fields)
 	def put(self, id):
 		args = self.reqparse.parse_args()
 		query = models.Todo.update(**args).where(models.Todo.id==id)
 		query.execute()
-		return (add_reviews(models.Todo.get(models.Todo.id==id)), 200,
+		return (models.Todo.get(models.Todo.id==id), 200,
 				{'Location': url_for('resources.todos.todo', id=id)})
 		
 	def delete(self, id):
 		query = models.Todo.delete().where(models.Todo.id==id)
 		query.execute()
-		return '', 204, {'Location': url_for('resources.todos.todos')}
+		return ('', 204, {'Location': url_for('resources.todos.todos')})
 		
-todos_api = Blueprint('resources.courses', __name__)
+todos_api = Blueprint('resources.todos', __name__)
 api = Api(todos_api)
 api.add_resource(
 	TodoList,
-	'/api/v1/todos',
+	'/todos',
 	endpoint='todos'
 )
 api.add_resource(
 	Todo,
-	'/api/v1/todos/<int:id>',
+	'/todos/<int:id>',
 	endpoint='todo'
 )
